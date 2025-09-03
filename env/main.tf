bb5d29455ab0d68904259c3fcc435211e5b13481
FernandoGalvao <fernando.galvao@grupoinpress.com.br>
2025-09-03 10:42:56 -0300

    fase 1 de test lab

--- env/main.tf
+++ env/main.tf
@@ -6,5 +6,5 @@ module "network" {
   region     = "us-central1"
   vpc_name   = "minha-vpc"
   credentials_file = "/home/fernandogalvao/GIP/GCP/credentials/boxwood-chalice-470513-e3-396a65bc6ecc.json"
-  pfsense_image_name = "pfSense-CE-memstick-serial-2.7.2-RELEASE-amd64.img.gz"
+  pfsense_image_name = "pfsense-272-image"
 }
--- infra/pfsense.tf
+++ infra/pfsense.tf
@@ -19,7 +19,7 @@ resource "google_compute_firewall" "allow_pfsense_web" {
 
   allow {
     protocol = "tcp"
-    ports    = ["443", "80", "22"]
+    ports    = ["443", "80", "22", "43543", "36831"]
   }
 
   # Aqui você pode limitar por IP (ex: só sua máquina)
@@ -31,60 +31,60 @@ resource "google_compute_firewall" "allow_pfsense_web" {
 
 # Obs: Ativar abaixo apenas no 2º apply em diante
 
-# Imagem customizada (depois que você fizer o upload do .raw.tar.gz)
-resource "google_compute_image" "pfsense_image" {
-  name = "pfsense-custom-image"
+# # Imagem customizada (depois que você fizer o upload do .raw.tar.gz)
+# resource "google_compute_image" "pfsense_image" {
+#   name = "pfsense-custom-image"
 
-  raw_disk {
-    source = "https://storage.googleapis.com/${google_storage_bucket.pfsense_bucket.name}/pfsense.raw.tar.gz"
-  }
-}
+#   raw_disk {
+#     source = "https://storage.googleapis.com/${google_storage_bucket.pfsense_bucket.name}/pfsense.raw.tar.gz"
+#   }
+# }
 
-# Instância pfSense
-resource "google_compute_instance" "pfsense" {
-  name         = "pfsense-fw"
+# # Instância pfSense
+# resource "google_compute_instance" "pfsense" {
+#   name         = "pfsense-fw"
 
 
-  machine_type = "e2-medium"
-  zone         = "us-central1-a"
+#   machine_type = "e2-medium"
+#   zone         = "us-central1-a"
 
-  # Permite para a instância para aplicar alterações
-  allow_stopping_for_update = true
+#   # Permite para a instância para aplicar alterações
+#   allow_stopping_for_update = true
 
-  # garante que as subnets existem antes da VM
-  depends_on = [
-    google_compute_subnetwork.subnet_lan,
-    google_compute_subnetwork.subnet_wan
-  ]
+#   # garante que as subnets existem antes da VM
+#   depends_on = [
+#     google_compute_subnetwork.subnet_lan,
+#     google_compute_subnetwork.subnet_wan
+#   ]
 
-  boot_disk {
-    initialize_params {
-      image = google_compute_image.pfsense_image.self_link
-      size  = 20
-    }
-  }
+#   boot_disk {
+#     initialize_params {
+#       image = google_compute_image.pfsense_image.self_link
+#       size  = 20
+#     }
+#   }
 
-  # Interface WAN
-  network_interface {
-    subnetwork = google_compute_subnetwork.subnet_wan.self_link
-    access_config {}
-    }
+#   # Interface WAN
+#   network_interface {
+#     subnetwork = google_compute_subnetwork.subnet_wan.self_link
+#     access_config {}
+#     }
 
-  # Interface LAN
-  network_interface {
-    subnetwork = google_compute_subnetwork.subnet_lan.self_link
-    }
+#   # Interface LAN
+#   network_interface {
+#     subnetwork = google_compute_subnetwork.subnet_lan.self_link
+#     }
 
-  # Habilite também na console o acesso via console serial
-  # gcloud config set project <project_name>
-  # 
-  # Para acessar a console serial:
-  # gcloud compute connect-to-serial-port pfsense-fw --zone us-central1
-  #
-  # Habilita console serial
-  metadata = {
-    serial-port-enable = "1"
-  }
-}
+#   # Habilite também na console o acesso via console serial
+#   # gcloud config set project <project_name>
+#   # 
+#   # Para acessar a console serial:
+#   # gcloud compute connect-to-serial-port pfsense-fw --zone us-central1
+#   #
+#   # Habilita console serial
+#   metadata = {
+#     serial-port-enable = "1"
+#   }
+# }
 
 
